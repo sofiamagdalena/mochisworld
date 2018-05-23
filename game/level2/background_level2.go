d51cde39807604240caa8dd4672743795296be75
Sofia Rytterlund <sofia.rytterlund@gmail.com>
2018-05-23 13:07:08 +0200

    iterate on level2

---
+++ game/level2/background_level2.go
@@ -0,0 +1,15 @@
+components {
+  id: "background_level2"
+  component: "/game/level2/background_level2.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
